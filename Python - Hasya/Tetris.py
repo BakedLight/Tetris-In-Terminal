b62a6b8f4ea280b0c42b97d1c7c6036d920e12d4
Cosmo <121879395+Cosmosynth@users.noreply.github.com>
2026-09-01 23:38:03 +0530

    Starting Screen (Testing)

--- Python - Hasya/Tetris.py
+++ Python - Hasya/Tetris.py
@@ -10,5 +10,26 @@ def main(stdscr):
     
     stdscr.nodelay(True) 
 
+    is_running = True
+    score = 0
+
+    while is_running:
+            key = stdscr.getch()
+            
+            if key == ord('q'):
+                is_running = False
+
+
+            stdscr.clear()
+                    
+                  
+            stdscr.addstr("Welcome to Terminal Tetris!\n")
+            stdscr.addstr(f"Score: {score}\n")
+            stdscr.addstr("Press 'q' to quit.\n")
+                    
+            stdscr.refresh() 
+                    
+
+
 if __name__ == "__main__":
     curses.wrapper(main)
