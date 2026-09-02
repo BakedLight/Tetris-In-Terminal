1778bf93eab25aaa117d1dc1fd7036e8bce4ed10
Hasya <patelhasya1512@gmail.com>
2026-09-02 10:42:06 +0530

    removed try,except block + small changes

--- Python - Hasya/Tetris.py
+++ Python - Hasya/Tetris.py
@@ -3,73 +3,57 @@ import time
 import random
 
 
+block_shapes = []
 
-def board_outline(stdscr, width=20, height=20):
-    left_side = "#|"
-    right_side = "|#"
 
-    
-    for y in range(height):
+def board_outline(stdscr):
 
-        try:
-            stdscr.addstr(y, 0, left_side + " " * width + right_side)
-        except curses.error:
-            pass 
-        
-    
-    bottom_border = "#|" + "#" * width + "|#"
-    try:
-        stdscr.addstr(height, 0, bottom_border)
-    except curses.error:
-        pass
-        
+
+    for y in range(20):
+        stdscr.addstr(y, 0, "#|")
+        stdscr.addstr(y, 22, "|#")
+
+    stdscr.addstr(20, 0, "#|" + "#" * 20 + "|#")
 
 
 def main(stdscr):
 
+
     curses.curs_set(0) # This  removes the blinking cursor :) noiceee 
-    
-    stdscr.nodelay(False) 
 
     score = 0
-
-    stdscr.clear()
-
-    stdscr.addstr(4, 5, "Welcome to Terminal Tetris!")
-    stdscr.addstr(7, 5, f"Score: {score}")
-    stdscr.addstr(8, 5, "Press ANY key to start (Don't press the power key BITCH)")
-    stdscr.addstr(9, 5, "Press 'q' to quit.")
-
-
-    stdscr.refresh() 
-
-
-    key = stdscr.getch()
-    if key == ord('q'):
-        return 
-
-
-    stdscr.clear()
-    
-    stdscr.nodelay(True)
     is_running = True
 
+    stdscr.addstr(5, 5, "Welcome to Terminal Tetris!!")
+    stdscr.addstr(7, 5, "Press ANY key to start (Don't press the power key BITCH)")
+    stdscr.addstr(8, 5, "Press q to quit")
+    stdscr.refresh()
+
+    key = stdscr.getch()
+
+    if key == ord("q"):
+        return
+
+
+    stdscr.clear()
+    stdscr.nodelay(True)
+
     while is_running:
+        stdscr.clear()
 
         board_outline(stdscr)
-        stdscr.refresh()
+
+        stdscr.addstr(2, 25, f"Score: {score}")
 
         key = stdscr.getch()
 
-        if key == ord('q'):
-            is_running = False
-            
-        time.sleep(0.05)
+        if key == ord("q"):
+            break
 
+        time.sleep(0.1)
+
+
+# curses.wrapper(main)
 
 if __name__ == "__main__":
     curses.wrapper(main)
-
-
-
-
