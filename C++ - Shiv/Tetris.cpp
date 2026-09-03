628b43128deb314f2c91ce761e7e18ddfab76a8d
Baked Light <briyanishiv1234@gmail.com>
2026-09-03 23:23:29 +0530

    Added input detection with arrow keys

--- C++ - Shiv/Tetris.cpp
+++ C++ - Shiv/Tetris.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
 using namespace std;
 
 void makeBox(int rows, int columns, int thickness) {
@@ -22,9 +24,39 @@ void makeBox(int rows, int columns, int thickness) {
     }
 }
 
+void gotoRow(int targetRow) {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    GetConsoleScreenBufferInfo(h, &info);
+    SetConsoleCursorPosition(h, { 0, (SHORT)targetRow });
+}   
+
 int main() {
     const int ROWS = 10, COLUMNS = 10, THICKNESS = 2;
-    makeBox(ROWS, COLUMNS, THICKNESS);
+    // makeBox(ROWS, COLUMNS, THICKNESS);
+
+    // Update Loop
+    while (true){
+        // Check if a key was pressed without blocking
+        if (_kbhit()) {
+            char key = getch(); // Get the char immediately
+            if (key == 'K') {
+                cout << "Left" << endl;
+            }
+            else if (key == 'M') {
+                cout << "Right" << endl;
+            }
+            else if (key == 'P') {
+                cout << "Down" << endl;
+            }
+            else if (key == 'H') {
+                cout << "Up" << endl;
+            }
+        }
+
+        // Other code here
+
+    }
 
     return 0;
 }
